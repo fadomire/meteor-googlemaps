c70d1affd557e3c540f2ca9f2b8309902e671904
Andrew Winter <awinter@sbstrm.co.jp>
2015-01-03 11:49:08 -0800

    Updating package.js
    Updating package.js for publishing.

--- package.js
+++ package.js
@@ -1,5 +1,8 @@
 Package.describe({
-        summary: "google maps repackaged for Meteor"
+	version: "0.1.1",
+	name: "drewjw81:googlemaps",
+	git: "https://github.com/drewjw81/meteor-googlemaps.git",
+        summary: "Google maps repackaged for Meteor"
 });
 
 Package.on_use(function (api, where) {
